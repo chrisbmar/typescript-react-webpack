9a6ece1b57ae65ede5ee11788f1a603793526c8f
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-20 23:06:40 +0200

    typescript config settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const root = document.querySelector("root");
+const root = document.getElementById("root");
 
 ReactDOM.render(<h1>Typescript, React & Webpack setup</h1>, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  entry: "src/index.tsx",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [],
 };
